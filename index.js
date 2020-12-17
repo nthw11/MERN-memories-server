e7fc704bfc111802e968b812f89ad8756f1b4ff0
Nathaniel Wallace <nathaniel.wallace@gmail.com>
2020-12-17 12:30:10 -0500

    moved app.get('/') higher up

--- index.js
+++ index.js
@@ -12,11 +12,12 @@ app.use(bodyParser.json({ limit: "30mb", extended: true}))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}))
 app.use(cors())
 
-app.use('/posts', postRoutes)
 
 app.get('/', (req, res) => {
   res.send('Hello to memories API')
 })
+
+app.use('/posts', postRoutes)
 // const CONNECTION_URL = 'mongodb+srv://nthw-test:kG0joZDUkptNGPSR@cluster0.4ot4n.mongodb.net/testdb?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000
 
